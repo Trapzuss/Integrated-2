ac31a41ee747d43765dc9521b374a7f7d1b9c732
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-05-23 18:57:35 +0700

    [ADD] integrate to api with dio

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:pet_integrated/widgets/registration_form.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _controllerUsername = TextEditingController();
+  final _controllerPassword = TextEditingController();
+  final _controllerDisplayName = TextEditingController();
+
+  @override
+  void dispose() {
+    _controllerUsername.dispose();
+    _controllerPassword.dispose();
+    _controllerDisplayName.dispose();
+    super.dispose();
+  }
+
+  Future<void> _submitForm() async {
+    try {
+      var payload = {
+        'userDisplayName': _controllerDisplayName.text,
+        'username': _controllerUsername.text,
+        'password': _controllerPassword.text,
+      };
+      if (_formKey.currentState!.validate()) {
+        var response = await Dio().post('http://10.0.2.2:3000/auth/register',
+            data: jsonEncode(payload));
+        log(response.statusCode.toString());
+        // if (response.statusCode == 200) {
+        //   log(response.toString());
+        //   ScaffoldMessenger.of(context).showSnackBar(
+        //     const SnackBar(content: Text('Processing Data')),
+        //   );
+        // } else {
+        //   log(response.toString());
+        // }
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: Container(
+              margin: EdgeInsets.only(top: 32),
+              child: registrationForm(
+                  formKey: _formKey,
+                  controllerUsername: _controllerUsername,
+                  controllerPassword: _controllerPassword,
+                  controllerDisplayName: _controllerDisplayName,
+                  submitForm: _submitForm)),
+        ));
+  }
+}
---
+++ lib/utils/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final colors = AppColors();
+  static final src = AppSrc();
+
+  const AppTheme._();
+
+  static ThemeData define() {
+    return ThemeData(primaryColor: const Color(0xFF126F63));
+  }
+}
+
+class AppColors {
+  final primary = const Color(0xFF126F63);
+  final primaryFontColor = const Color(0xFF202020);
+  final secondaryFontColor = const Color(0xFFFFFCF1);
+  final darkFontColor = const Color(0xFF263238);
+}
+
+class AppSrc {}
