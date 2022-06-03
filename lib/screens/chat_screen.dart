f83dc1997de3395fd8560da6ee94e7476e0259c9
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-04 01:52:55 +0700

    Update chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,9 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:pet_integrated/utils/theme.dart';
 import 'package:pet_integrated/widgets/chat/detail_chat_appbar.dart';
 import 'package:pet_integrated/widgets/chat/detail_chat_contact_info.dart';
 import 'package:pet_integrated/widgets/chat/detail_chat_messages.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:uuid/uuid.dart';
+import 'package:username_gen/username_gen.dart';
+import '../models/message.dart';
+import '../models/user.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -15,34 +22,81 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   late IO.Socket socket;
   final _messageController = TextEditingController();
+  List<MessageModel> messages = [];
+
   @override
   void initState() {
+    connectSocket();
     super.initState();
-    connect();
   }
 
-  void connect() {
-    // http://localhost:3000
-    // http://10.0.2.2:3000
-    // http://192.168.0.193:3000
-    // Dart client
-    IO.Socket socket = IO.io('http://10.0.2.2:3000', <String, dynamic>{
-      'transports': ['websocket'],
-      'autoConnect': true,
+  @override
+  void dispose() {
+    socket
+        .disconnect(); // --> disconnects the Socket.IO client once the screen is disposed
+    super.dispose();
+  }
+
+  void connectSocket() {
+    var url =
+        Platform.isAndroid ? 'http://10.0.2.2:4000' : 'http://localhost:4000';
+    socket = IO.io(
+        'http://10.0.2.2:4000',
+        IO.OptionBuilder()
+            .setTransports(['websocket']) // for Flutter or Dart VM
+            .disableAutoConnect() // disable auto-connection
+            .build());
+    socket.connect();
+    socket.onConnect((data) {
+      print('Connected to socket.io server');
+      socket.emit('join', 'join from client');
+      socketListener();
+      // socket.emit('sign-in', myId);
     });
-    socket.onConnect((_) {
-      print('connect');
-      socket.emit('msg', 'test');
+    socket.onDisconnect((data) {
+      print('disconnected');
     });
-    print(socket.connected);
-    socket.on('findAllMessages', (data) => print(data));
-    socket.onDisconnect((_) => print('disconnect'));
-    socket.on('fromServer', (_) => print(_));
+
+    socket.on('fromServer', (data) => print(data));
+  }
+
+  socketListener() {
+    socket.on('received-messages', (data) {
+      setState(() {
+        messages = data
+            .map<MessageModel>((json) => MessageModel.fromJson(json))
+            .toList();
+      });
+      print('update');
+      // print(messages);
+    });
   }
 
   void _sendMessage() {
-    print(_messageController.text);
-    socket.emit('createMessage', _messageController.text);
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('Hms').format(now);
+    UserModel user = UserModel(
+        Uuid().v1(),
+        '',
+        UsernameGen.generateWith(
+          data: UsernameGenData(
+            names: ['Curry', 'Thai', 'Land'],
+            adjectives: ['Yo', 'Sup', 'Hi'],
+          ),
+        ),
+        'Lastname',
+        '',
+        Color(0xFFFDBEC8));
+    var messageJson = {
+      "user": user.toJson(),
+      "lastMessage": _messageController.text,
+      "lastTime": formattedDate,
+      "isContinue": false,
+    };
+
+    socket.emit('createMessage', messageJson);
+    _messageController.clear();
+    socketListener();
   }
 
   @override
@@ -61,10 +115,18 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: [
           Expanded(
-              child: DetailMessages(
-            sendMessage: _sendMessage,
-            messageController: _messageController,
-          ))
+              child: socket.connected
+                  ? DetailMessages(
+                      // refresh: _fetchMessage,
+                      messages: messages,
+                      sendMessage: _sendMessage,
+                      messageController: _messageController,
+                    )
+                  : Container(
+                      child: Center(
+                        child: Text('Connecting...'),
+                      ),
+                    )),
         ],
       ),
     );
--- lib/widgets/chat/detail_chat_messages.dart
+++ lib/widgets/chat/detail_chat_messages.dart
@@ -1,23 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:pet_integrated/models/message.dart';
 import 'package:pet_integrated/utils/theme.dart';
 
 class DetailMessages extends StatefulWidget {
-  DetailMessages(
-      {Key? key, required this.messageController, required this.sendMessage})
-      : super(key: key);
+  DetailMessages({
+    Key? key,
+    required this.messageController,
+    required this.sendMessage,
+    required this.messages,
+    // required this.refresh
+  }) : super(key: key);
   var messageController = TextEditingController();
+  List<MessageModel> messages;
   Function sendMessage;
+  // Function refresh;
+
   @override
   State<DetailMessages> createState() => _DetailMessagesState();
 }
 
 class _DetailMessagesState extends State<DetailMessages> {
   final _scrollController = ScrollController();
+  final List<MessageModel> list = MessageModel.generateMessages();
+  final String myId = "0";
+  List<MessageModel> _message = [];
+
+  _fetchMessage() {
+    if (widget.messages.length != _message.length) {
+      // setState(() {
+      _message = widget.messages;
+      // });
+    }
+
+    // print(widget.messages);
+    print('fetch message in child');
+    print(_message);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMessage();
+  }
+
+  @mustCallSuper
+  @protected
+  void didUpdateWidget(widget) {
+    super.didUpdateWidget(widget);
+    _fetchMessage();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Stack(children: [
         Container(
+            child: ChatListView(),
             margin: EdgeInsets.only(top: 20),
             padding: EdgeInsets.fromLTRB(25, 50, 25, 80),
             decoration: BoxDecoration(
@@ -30,6 +73,7 @@ class _DetailMessagesState extends State<DetailMessages> {
           child: Container(
             margin: EdgeInsets.only(bottom: 20),
             height: 50,
+            padding: EdgeInsets.symmetric(horizontal: 10),
             child: Stack(children: [
               TextField(
                 controller: widget.messageController,
@@ -68,6 +112,85 @@ class _DetailMessagesState extends State<DetailMessages> {
           ),
         )
       ]),
+    );
+  }
+
+  Widget ChatListView() {
+    return ListView.separated(
+        padding: EdgeInsets.zero,
+        itemBuilder: ((context, index) => _message[index].user.id == myId
+            ? _buildReceivedText(_message[index])
+            : _buildSenderText(_message[index])),
+        separatorBuilder: (_, index) => SizedBox(
+              height: 1,
+            ),
+        itemCount: _message.length);
+  }
+
+  _buildReceivedText(message) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          message.lastTime,
+          style: TextStyle(color: AppTheme.colors.primary, fontSize: 12),
+        ),
+        Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                    bottomLeft: Radius.circular(15))),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 180),
+              child: Text(message.lastMessage),
+            ))
+      ],
+    );
+  }
+
+  _buildSenderText(message) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                  color: Colors.grey[300], shape: BoxShape.circle),
+              width: 40,
+              child: Icon(Icons.person),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.1),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                      bottomRight: Radius.circular(15))),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 180),
+                child: Text(
+                  message.lastMessage,
+                  style: TextStyle(
+                      height: 1.5, color: AppTheme.colors.darkFontColor),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Text(
+          message.lastTime,
+          style: TextStyle(color: AppTheme.colors.primary, fontSize: 12),
+        ),
+      ],
     );
   }
 }
