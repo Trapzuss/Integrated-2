ecddb7460034f550e809939fe8388ee3a6e154b9
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-04 01:52:45 +0700

    Update apptheme

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -3,20 +3,45 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static final colors = AppColors();
   static final src = AppSrc();
+  static final style = AppStyle();
 
   const AppTheme._();
 
   static ThemeData define() {
-    return ThemeData(primaryColor: const Color(0xFF126F63));
+    return ThemeData(primaryColor: const Color(0xFFACBD86));
   }
 }
 
 class AppColors {
-  final primary = const Color(0xFF126F63);
+  // final primary = const Color(0xFF126F63);
+  final primary = const Color(0xFFACBD86);
   final primaryFontColor = const Color(0xFF202020);
   final secondaryFontColor = const Color(0xFFFFFCF1);
   final darkFontColor = const Color(0xFF263238);
   final notWhite = const Color.fromARGB(255, 235, 235, 235);
+  final infoFontColor = Color.fromARGB(255, 113, 113, 113);
+  final subInfoFontColor = Color.fromARGB(255, 152, 152, 152);
+  // final linearColors = LinearGradient(
+  //   transform: GradientRotation(270),
+  //   colors: [
+  //     Color.fromARGB(255, 255, 255, 248),
+  //     AppTheme.colors.secondaryFontColor
+  //   ],
+  //   begin: Alignment.topLeft,
+  //   end: Alignment.bottomRight,
+  // );
+}
+
+class AppStyle {
+  final noButtonElevation = MaterialStateProperty.resolveWith<double>(
+    // As you said you dont need elevation. I'm returning 0 in both case
+    (Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        return 0;
+      }
+      return 0; // Defer to the widget's default.
+    },
+  );
 }
 
 class AppSrc {}
