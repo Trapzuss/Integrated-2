49cf9bccad4b324a1e32b35719565cb8e86ed652
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-04 01:53:30 +0700

    Update mess model

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,6 +1,34 @@
-class MessageModel {
-  final String name;
-  final String text;
+import 'package:pet_integrated/models/user.dart';
 
-  const MessageModel({required this.name, required this.text});
+class MessageModel {
+  UserModel user;
+  String lastMessage;
+  String lastTime;
+  bool isContinue;
+  MessageModel(this.user, this.lastMessage, this.lastTime,
+      {this.isContinue = false});
+
+  static List<MessageModel> generateMessages() {
+    return [
+      MessageModel(users[0], 'Hey', '18:32'),
+      MessageModel(users[1], 'Ye', '20.11'),
+      MessageModel(users[0], 'Yo', '20.12'),
+    ];
+  }
+
+  Map<String, dynamic> toJson() => {
+        'user': user.toJson(),
+        'lastMessage': lastMessage,
+        'lastTime': lastTime,
+        'isContinue': isContinue,
+      };
+
+  factory MessageModel.fromJson(Map<String, dynamic> json) => MessageModel(
+        UserModel.fromJson(json['user']),
+        json['lastMessage'],
+        json['lastTime'],
+        isContinue: json['isContinue'],
+      );
 }
+
+var users = UserModel.generateUsers();
