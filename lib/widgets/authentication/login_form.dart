fc69249d28c7c1a0410bbb3c77e5a1faf0e01830
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-12 19:28:59 +0700

    ADD authentication form

--- lib/widgets/authentication/login_form.dart
+++ lib/widgets/authentication/login_form.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:pet_integrated/layouts/default_layout.dart';
+import 'package:pet_integrated/utils/theme.dart';
 
-class registrationForm extends StatefulWidget {
+class loginForm extends StatefulWidget {
   var formKey = GlobalKey<FormState>();
   var controllerUsername = TextEditingController();
   var controllerPassword = TextEditingController();
-  var controllerDisplayName = TextEditingController();
+
   Function submitForm;
-  registrationForm(
+  loginForm(
       {Key? key,
       required this.formKey,
       required this.controllerUsername,
       required this.controllerPassword,
-      required this.controllerDisplayName,
       required this.submitForm})
       : super(key: key);
 
   @override
-  State<registrationForm> createState() => _registrationFormState();
+  State<loginForm> createState() => _loginFormState();
 }
 
-class _registrationFormState extends State<registrationForm> {
+class _loginFormState extends State<loginForm> {
+  String? _usernameRules(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'username is required';
+    } else if (value != null && value.contains('@')) {
+      return 'Do not use the @ char.';
+    }
+  }
+
+  String? _passwordRules(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'password is required';
+    } else if (value != null && value.contains('@')) {
+      return 'Do not use the @ char.';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -31,6 +48,7 @@ class _registrationFormState extends State<registrationForm> {
             Container(
               margin: EdgeInsets.symmetric(vertical: 8),
               child: TextFormField(
+                validator: _usernameRules,
                 controller: widget.controllerUsername,
                 decoration: InputDecoration(
                   filled: true,
@@ -55,35 +73,12 @@ class _registrationFormState extends State<registrationForm> {
             Container(
               margin: EdgeInsets.symmetric(vertical: 8),
               child: TextFormField(
+                validator: _passwordRules,
                 controller: widget.controllerPassword,
                 obscureText: true,
                 decoration: InputDecoration(
                   filled: true,
                   labelText: "Enter Password",
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.0),
-                    borderSide: BorderSide(
-                      color: Colors.blue,
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.0),
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 2.0,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 8),
-              child: TextFormField(
-                controller: widget.controllerDisplayName,
-                decoration: InputDecoration(
-                  filled: true,
-                  labelText: "Enter DisplayName",
                   fillColor: Colors.white,
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(25.0),
@@ -108,12 +103,32 @@ class _registrationFormState extends State<registrationForm> {
                   widget.submitForm(context);
                 },
                 icon: Icon(Icons.arrow_right),
-                label: Text('Sign up'),
+                label: Text('Sign in'),
                 style: ButtonStyle(
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                         RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(18.0),
                 ))),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => DefaultLayout()));
+                },
+                icon: Icon(Icons.person),
+                label: Text('Continue as guest'),
+                style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        AppTheme.colors.primary),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: AppTheme.colors.primary)))),
               ),
             ),
           ],
