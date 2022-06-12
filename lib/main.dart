38e692c45f3341cd300cccdb5fe064fddde63dc9
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-12 19:28:22 +0700

    Update authentication wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pet_integrated/layouts/default_layout.dart';
+import 'package:pet_integrated/mixins/authentication.dart';
 
 import 'package:pet_integrated/screens/chat/chat_screen.dart';
-import 'package:pet_integrated/screens/auth/login_screen.dart';
+import 'package:pet_integrated/screens/auth/register_screen.dart';
 import 'package:pet_integrated/screens/splash_screen.dart';
 import 'package:pet_integrated/utils/theme.dart';
 import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 import 'package:bot_toast/bot_toast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:cookie_jar/cookie_jar.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var dio = Dio();
+  var cookieJar = CookieJar();
+  dio.interceptors.add(CookieManager(cookieJar));
+  await dotenv.load(fileName: ".env");
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var access_token = prefs.get('access_token');
+  if (access_token != null) {
+    await AuthenticationServices.getProfile();
+  }
+
   runApp(const MyApp());
 }
 
