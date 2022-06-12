4cba0dcc8a46fcdd7934d83809de3e744396b7b5
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-12 19:27:03 +0700

    Update authentication screens

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -3,7 +3,16 @@ import 'dart:developer';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:pet_integrated/layouts/default_layout.dart';
+import 'package:pet_integrated/mixins/authentication.dart';
+import 'package:pet_integrated/screens/auth/register_screen.dart';
+import 'package:pet_integrated/screens/posts/post_screen.dart';
+import 'package:pet_integrated/screens/profile/profile_screen.dart';
+import 'package:pet_integrated/utils/theme.dart';
+import 'package:pet_integrated/widgets/authentication/login_form.dart';
 import 'package:pet_integrated/widgets/authentication/registration_form.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -16,36 +25,35 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   final _controllerUsername = TextEditingController();
   final _controllerPassword = TextEditingController();
-  final _controllerDisplayName = TextEditingController();
 
   @override
   void dispose() {
     _controllerUsername.dispose();
     _controllerPassword.dispose();
-    _controllerDisplayName.dispose();
+
     super.dispose();
   }
 
-  Future<void> _submitForm() async {
+  Future<void> _submitForm(context) async {
     try {
-      var payload = {
-        'userDisplayName': _controllerDisplayName.text,
-        'username': _controllerUsername.text,
-        'password': _controllerPassword.text,
-      };
-      if (_formKey.currentState!.validate()) {
-        var response = await Dio().post('http://10.0.2.2:3000/auth/register',
-            data: jsonEncode(payload));
-        log(response.statusCode.toString());
-        // if (response.statusCode == 200) {
-        //   log(response.toString());
-        //   ScaffoldMessenger.of(context).showSnackBar(
-        //     const SnackBar(content: Text('Processing Data')),
-        //   );
-        // } else {
-        //   log(response.toString());
-        // }
-      }
+      final isValid = _formKey.currentState!.validate();
+      if (isValid) {
+        var payload = {
+          'username': _controllerUsername.text,
+          'password': _controllerPassword.text,
+        };
+
+        await AuthenticationServices.login(payload);
+        await AuthenticationServices.getProfile();
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Processing Data')),
+        );
+
+        await Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => DefaultLayout()));
+      } else
+        return;
     } catch (e) {
       log(e.toString());
     }
@@ -59,13 +67,70 @@ class _LoginScreenState extends State<LoginScreen> {
         resizeToAvoidBottomInset: false,
         body: SingleChildScrollView(
           child: Container(
-              margin: EdgeInsets.only(top: 32),
-              child: registrationForm(
-                  formKey: _formKey,
-                  controllerUsername: _controllerUsername,
-                  controllerPassword: _controllerPassword,
-                  controllerDisplayName: _controllerDisplayName,
-                  submitForm: _submitForm)),
+              margin: EdgeInsets.only(top: 32, left: 10, right: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Container(
+                      width: width * 0.3,
+                      height: height * 0.3,
+                      child: CircleAvatar(
+                        backgroundColor: AppTheme.colors.primary,
+                        foregroundColor: AppTheme.colors.notWhite,
+                        child: Icon(
+                          Icons.pets,
+                          size: 54,
+                        ),
+                      ),
+                    ),
+                  ]),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Text(
+                      'Pethome',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: AppTheme.colors.primary),
+                    ),
+                  ]),
+                  Text(
+                    'Sign In',
+                    textAlign: TextAlign.left,
+                    style: AppTheme.style.titleFontStyle,
+                  ),
+                  loginForm(
+                      formKey: _formKey,
+                      controllerUsername: _controllerUsername,
+                      controllerPassword: _controllerPassword,
+                      submitForm: _submitForm),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Not registered?',
+                        style: AppTheme.style.secondaryFontStyle,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterScreen()));
+                        },
+                        child: Text(
+                          'Create an register',
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: AppTheme.colors.decorateColor),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )),
         ));
   }
 }
