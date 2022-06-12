6291e7093e6a97e696eada7248e76a5e4b9805e7
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-13 03:44:39 +0700

    UPDATE history screen

--- lib/screens/profile/profile_history_screen.dart
+++ lib/screens/profile/profile_history_screen.dart
@@ -19,6 +19,100 @@ class _ProfileHistoryScreenState extends State<ProfileHistoryScreen> {
         backgroundColor: AppTheme.colors.notWhite,
         elevation: 0,
       ),
+      body: SafeArea(
+        child: Container(
+            margin: EdgeInsets.only(right: 10, left: 10),
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) => ListTile(
+                  // tileColor: Colors.white,
+                  contentPadding: EdgeInsets.only(left: 0, right: 0),
+                  minLeadingWidth: 60,
+                  leading: ClipRRect(
+                    child: Image.network(
+                      'https://picsum.photos/200/300?random=${index}',
+                      fit: BoxFit.cover,
+                      width: 60,
+                    ),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 4),
+                        child: Text(
+                          'Pet ${index}',
+                          style: AppTheme.style.titleFontStyle,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 4),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: Colors.red[300],
+                              size: 16,
+                            ),
+                            Text(
+                              '10600 Bangkok',
+                              style: AppTheme.style.secondaryFontStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 4),
+                        child: Text(
+                          'Confirm Adopt',
+                          style: AppTheme.style.primaryFontStyle,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 4),
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(right: 5),
+                              child: CircleAvatar(
+                                radius: 10,
+                                child: Icon(
+                                  Icons.person,
+                                  size: 14,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'hello ${index}',
+                              style: AppTheme.style.primaryFontStyle,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  trailing: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 4),
+                        child: Text(
+                          '3000 Baht',
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: AppTheme.colors.primary),
+                        ),
+                      ),
+                      Text(
+                        '6/10/2022',
+                        style: AppTheme.style.secondaryFontStyle,
+                      ),
+                    ],
+                  )),
+            )),
+      ),
     );
   }
 }
