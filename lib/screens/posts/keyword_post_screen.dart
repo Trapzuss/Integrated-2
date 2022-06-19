5a82e6c5b822ba0036d82689a5644278d29089aa
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-19 19:59:22 +0700

    ADD keyword post screen

---
+++ lib/screens/posts/keyword_post_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:pet_integrated/services/posts.dart';
+import 'package:pet_integrated/utils/theme.dart';
+import 'package:pet_integrated/widgets/home/post_grid_view.dart';
+
+class KeywordPostScreen extends StatefulWidget {
+  var posts;
+  String keyword;
+  KeywordPostScreen({Key? key, required this.posts, required this.keyword})
+      : super(key: key);
+
+  @override
+  State<KeywordPostScreen> createState() => _KeywordPostScreenState();
+}
+
+class _KeywordPostScreenState extends State<KeywordPostScreen> {
+  Future _refreshPosts() async {
+    await PostServices.getPosts();
+    setState(() {});
+    // print('refresh success');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.keyword),
+          elevation: 0,
+          backgroundColor: AppTheme.colors.notWhite,
+        ),
+        body: Container(
+            margin: EdgeInsets.only(right: 10, left: 10),
+            child: PostGridView(
+                posts: widget.posts, refreshPosts: _refreshPosts)));
+  }
+}
