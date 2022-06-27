cf7851c17c5518f4a1cac12cf0bc48389d1bb0f6
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-27 19:13:54 +0700

    UPDATE apptheme, utils

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,6 @@
 import 'package:intl/intl.dart';
 import 'dart:convert';
+import 'package:timeago/timeago.dart' as Timeago;
 
 class ExtensionServices {
   static String capitalize(String value) {
@@ -15,5 +16,33 @@ class ExtensionServices {
     // print('formatted: $formatted');
 
     return formatted;
+  }
+
+  static convertTimestampToTime(timestamp) {
+    DateTime time = DateTime.parse(timestamp);
+    var formatter = new DateFormat('Hms');
+    var formatted = formatter.format(time);
+    // print('formatted: $formatted');
+
+    return formatted;
+  }
+
+  static convertTimestampToTimeago(timestamp) {
+    final now = new DateTime.now();
+    DateTime time = DateTime.parse(timestamp);
+    final difference = now.difference(time);
+    var ago = Timeago.format(now.subtract(difference),
+        locale: 'en_short', allowFromNow: true);
+
+    return ago;
+    // DateTime time = DateTime.parse(timestamp);
+    // final now = DateTime.now();
+    // final different = now.difference(time);
+
+    // return Timeago.format(
+    //   different,
+    //   locale: 'en_short',
+    //   allowFromNow: true,
+    // );
   }
 }
